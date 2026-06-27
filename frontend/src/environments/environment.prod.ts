b02cde1df55f1fc87e8fb8cd13ba5fae96b8603e
johnnypazH <johnny_ali@live.com>
2026-06-27 13:34:55 -0300

    config: aponta api de producao para a URL do Render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sua-api-de-producao.com' // <-- Substitua pela URL real da sua API
+  apiUrl: 'https://invix.onrender.com/api'
 };
